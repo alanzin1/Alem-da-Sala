810f1fb1c37d2743417f11ad2d907d285f47b6b9
alanzin1 <alanvictorapt@gmail.com>
2025-11-05 04:38:23 -0300

    fix b for B in NavBar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,5 +1,4 @@
-import { useState } from "react";
-import styles from "./Navbar.module.css";
+import styles from "./NavBar.module.css";
 
 export default function NavBar({ tags, selectedTags, onFilterChange }) {
   const handleTagClick = (tag) => {
